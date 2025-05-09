4fb89728411d24a97688a1c9514f4d18a5f9b3f1
Lehmol <chalottelinnea@hotmail.com>
2025-05-09 10:00:40 +0200

    deleted img and adding more to style

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -12,14 +12,14 @@ function MainPage() {
     },
     {
       image: "./src/svgs/christmasquiz.png",
-      description: "Christmas Trivia built in vanilla Javascript",
+      description: "Christmas quiz built in vanilla Javascript",
       liveLink: "https://christmas-triviaa.netlify.app/",
       codeLink: "https://github.com/Lehmol/Christmas-quiz",
     },
     {
       image: "./src/svgs/weatherapp.png",
       description:
-        "Weather application built with vanilla Javascript. The first application i've ever built.",
+        "The first application i've ever built. Weather application built with vanilla Javascript.",
       liveLink: "https://weatherappbylinnea.netlify.app/",
       codeLink: "https://github.com/Lehmol/To-do-app",
     },
@@ -52,32 +52,35 @@ function MainPage() {
           id="about"
           className="w-full flex flex-col items-center justify-center text-center px-4 py-10"
         >
-          <p className="m-4">About me</p>
+          <p className="m-4 text-lg font-semibold">About me</p>
           <aside className="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-10 p-4 max-w-5xl">
             <div className="bg-white shadow-md rounded-lg p-6 w-full">
-              <p className="tracking-wide">
+              <p className="tracking-wide text-lg">
                 I grew up in Falun, Dalarna and moved to Stockholm in 2016. Not
                 knowing exactly what I wanted to do, I explored{" "}
                 <b>science, math, and physics</b>—but it was <b>programming</b>{" "}
-                that really got me hooked. Today, I'm a{" "}
+                that really got me hooked.<br></br> Today, I'm a{" "}
                 <b>frontend developer</b> in the making, passionated about
                 building intuitive and accessible user interfaces. I’ve learned
                 the basic technologies like{" "}
                 <b>HTML, CSS, JavaScript, React, and Node.js</b>, and I love
-                combining logic with creativity. I’ve built both school and
-                personal projects where I’ve focused on responsive design, clean
-                code, and user experience. I'm also a strong communicator who
-                enjoys collaborating in teams and solving real-world problems
-                through tech, especially when it comes to <b>elder care</b>. I’m
-                now looking for opportunities to grow as a developer — whether
-                through internships, junior roles, or freelance projects.
+                combining logic with creativity. Along the way, I’ve been
+                learning how important things like responsive design, clean
+                code, and good user experience really are - and there's always
+                more to learn. <br></br> What keeps me going is the excitement
+                of turning <b>big ideas into real things</b> through code. I
+                love that with the skills I’m building, I can take an idea in my
+                head and actually bring it to life. Right now, I’m looking for
+                opportunities—whether it’s an internship, junior role, or
+                freelance project—to keep{" "}
+                <b>learning, growing, and creating.</b>
               </p>
             </div>
           </aside>
           <div>
             <div>
-              <p className="m-4">My skills</p>
-              <ul className="w-full h-[50px] bg-white flex items-center justify-between gap-4 px-4">
+              <p className="m-4 text-lg font-semibold">My skills</p>
+              <ul className="w-full bg-white flex flex-wrap items-center justify-center gap-10 px-4 py-4">
                 {[
                   "html.png",
                   "css (2).png",
@@ -92,7 +95,7 @@ function MainPage() {
                     <img
                       src={`./src/svgs/${src}`}
                       alt="Skill icon"
-                      className="w-full h-full object-contain"
+                      className="w-full h-full object-contain transition-transform duration-300 ease-in-out hover:scale-110"
                     />
                   </li>
                 ))}
@@ -104,9 +107,7 @@ function MainPage() {
           id="projects"
           className="w-full flex flex-col items-center justify-center text-center px-4 py-10"
         >
-          <p className="m-4" text-xl font-semibold tracking-wide>
-            Projects
-          </p>
+          <p className="m-4 text-xl font-semibold tracking-wide">Projects</p>
           <div className="grid gap-6 md:grid-cols-2">
             {projects.map((project, index) => (
               <Card
@@ -118,13 +119,43 @@ function MainPage() {
               />
             ))}
           </div>
+          <p className="m-4 text-l font-semibold tracking-wide">
+            More projects are coming soon...
+          </p>
         </section>
         <section
           id="contact"
           className="w-full flex flex-col items-center justify-center text-center px-4 py-10"
         >
-          <p className="m-4">Contact Page</p>
-          <div></div>
+          <p className="m-4 text-xl font-semibold tracking-wide">Contact</p>
+          <div>
+            <ul className="flex space-x-10 text-sm font-medium">
+              <li>
+                <a
+                  className="hover:underline"
+                  href="https://www.linkedin.com/in/linn%C3%A9a-lehmann-molander-554160328/"
+                >
+                  LinkedIn
+                </a>
+              </li>
+              <li>
+                <a className="hover:underline" href="https://github.com/Lehmol">
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  className="hover:underline"
+                  href="mailto:linnealehmann7@gmail.com"
+                >
+                  Email
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button className="m-4 bg-green-400 rounded h-10 w-40 shadow hover:bg-green-500">
+            Download CV(PDF)
+          </button>
         </section>
       </main>
       <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
@@ -138,43 +169,6 @@ function MainPage() {
           <a href="#contact" className="link link-hover">
             Contact
           </a>
-        </nav>
-        <nav>
-          <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
-          </div>
         </nav>
         <aside>
           <p>Copyright © {new Date().getFullYear()}</p>
